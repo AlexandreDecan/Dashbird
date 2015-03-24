170add8d3e0fe8f9d3ba596b8067258d757b08f3
Alexandre Decan <alexandre.decan@lexpage.net>
2015-03-24 08:50:01 +0100

    "/" redirige vers l'admin.

--- Dashbird/urls.py
+++ Dashbird/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     url(r'^gradmin/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index')), name='index'),
     url(r'^', include('dashboard.urls')),
 )
